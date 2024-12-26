6f5b709a74d0dff95fdf4f93fc77e1aa9f57fd2e
Behrouz <behjaf@gmail.com>
2024-12-26 21:17:16 +0330

    new

--- run_command.py
+++ run_command.py
@@ -118,9 +118,12 @@ if __name__ == "__main__":
                 command_text = command['command_text']
                 print(f"Command: {command_text}")
 
-                result = subprocess.run(command_text, shell=True, capture_output=True, text=True, check=True, timeout=15)
-
-                update_database(api_url, token, command['id'], result.stdout)
+                result = subprocess.run(command_text, shell=True, capture_output=True, text=True, check=True,
+                                        timeout=15)
+                result_text = result.stdout
+                if result_text == '':
+                    result_text = '[without result]'
+                update_database(api_url, token, command['id'], result_text)
 
 
 
